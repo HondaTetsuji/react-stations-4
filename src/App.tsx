ea5d8edbc7d439aa0919986ef893f1cfe4123665
HondaTetsuji <t-honda@gikentrastem.com>
2024-09-17 16:04:24 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const profile = {
+    name : "本田哲司"
+  } ;
+
+  return (
+    <div id="root">
+      <main className="profile">
+        <h2>プロフィール</h2>
+        <div className="profile-form">
+          <div className="profile-name">
+            名前 : {profile.name}
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
